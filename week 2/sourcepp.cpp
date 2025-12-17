ee46f38d05d6b634007f9e5caa5ec05973d2c343
itxfaizakhan250-art <itx.faizakhan.250@gmail.com>
2025-12-17 13:54:50 +0500

    Add files via upload

--- week 2/sourcepp.cpp
+++ week 2/sourcepp.cpp
@@ -1,10 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-	cout<<"*"<<endl;
-	cout<<"*""*"<<endl;
-	cout<<"*""*""*"<<endl;
-	cout<<"*""*""*""*"<<endl;
-	cout<<"*""*""*""*""*"<<endl;
+	cout<<"Iteam\tPrice\tQuality"<<endl;
+	cout<<"Pen\t$1\t10"<<endl;
+	cout<<"Pencil\t$2\t5"<<endl;
+	cout<<"Eraser\t$0.5\t20"<<endl;
 	return 0;
 }
