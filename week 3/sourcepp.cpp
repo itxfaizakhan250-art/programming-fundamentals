5bbf04e6218823fce7c74183d852b4b4176ebd55
itxfaizakhan250-art <itx.faizakhan.250@gmail.com>
2025-12-17 13:57:23 +0500

    Add files via upload

---
+++ week 3/sourcepp.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x1=3;
+	int x2=6;
+	int y1=4;
+	int y2=8;
+	int squared_distance
+	squared_distance=(x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
+	cout<<" the square distance between the points (3,4) and (6,8) is: "<<squared_distance<<endl;
+    return 0;
+}
