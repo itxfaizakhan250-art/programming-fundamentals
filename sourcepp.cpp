d63f7f5cf2842474501f4a26ad0f611aa9b41b56
itxfaizakhan250-art <itx.faizakhan.250@gmail.com>
2025-12-17 13:55:44 +0500

    Add files via upload

---
+++ sourcepp.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"she said, \"lets learn c++!\"\n";
+	cout<<"To start, save your file in C:\\C++_project\\lab1\\n";
+	cout<<"Good luck!\n";
+	return 0;
+}
+
